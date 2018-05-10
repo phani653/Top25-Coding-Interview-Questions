7a39737e2c2008edf4cebe82d3da446974493221
JOGI VENKATA PHANI KUMAR <phani653@gmail.com>
2018-05-10 07:56:08 +0530

    Update 14.1 Delete nth node form last, in linked list.cpp

--- 14.1 Delete nth node form last, in linked list.cpp
+++ 14.1 Delete nth node form last, in linked list.cpp
@@ -24,3 +24,20 @@ ListNode* removeNthFromEnd(ListNode* head, int n) {
         free(tmp);
         return head;
     }
+/*
+Here if we add dummy node infront of the list, we need to check all boundary cases
+    
+    ListNode dummy = new ListNode(0);
+    dummy.next = head;
+    ListNode first = dummy;
+    ListNode second = dummy;
+    for (int i = 1; i <= n + 1; i++) {
+        first = first.next;
+    }
+    while (first != null) {
+        first = first.next;
+        second = second.next;
+    }
+    second.next = second.next.next;
+    return dummy.next;
+ */
